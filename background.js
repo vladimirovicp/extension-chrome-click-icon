133d6bb0537bf5d2e66ea8bc3b13f3fd19aee6d4
vladimirovicp <vladimirovicp70@gmail.com>
2023-10-10 11:06:07 +0400

    ver 1

--- background.js
+++ background.js
@@ -1,63 +1,24 @@
-
-
-//https://developer.chrome.com/docs/extensions/reference/runtime/
-//https://www.youtube.com/watch?v=zNswnpCKuzU
-
-
-
 chrome.commands.onCommand.addListener((command) => {
-    console.log(command);
-
-    if(command === 'start-script'){
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            var activeTab  = tabs[0];
-            console.log('activeTab', activeTab)
-            chrome.tabs.sendMessage(activeTab.id, {msg: "demo"}, function(response) {
-                console.log(response);
-            });
-        });
-
-        // getCurrentTabId().then((tabId) =>{
-        //     //console.log(tabId)
-        //     //id получил
-        //
-        //     chrome.tabs.sendMessage(tabId, {greeting: "start-script"}, function(response) {
-        //         if (chrome.runtime.lastError) {
-        //             console.log("chrome.runtime.lastError " + chrome.runtime.lastError.message);
-        //         }
-        //         console.log("event page got a response " + response);
-        //         //console.log(response.farewell);
-        //
-        //         //alert("event page got a response" + response);
-        //     });
-        //
-        //
-        //     // chrome.tabs.sendMessage(tabId, {greeting: "start-script"}, function(response) {
-        //     //     if (!chrome.runtime.lastError) {
-        //     //         console.log('response', response);
-        //     //     }else{
-        //     //         console.log(chrome.runtime.lastError);
-        //     //     }
-        //     // });
-        //
-        //
-        //     // chrome.tabs.sendMessage(tabId, {action: 'copy-all'},
-        //     //     (allCode)=>{
-        //     //         console.log(allCode)
-        //     //     });
-        //
-        // })
-
-    }
-
-
+    if(command === 'start-script'){sendingContent();}
 });
 
+chrome.action.onClicked.addListener(sendingContent);
 
 async function getCurrentTabId() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab.id;
+}
+
+function sendingContent(){
+    getCurrentTabId().then((tabId) =>{
+        //id получил
+        chrome.tabs.sendMessage(tabId, {greeting: "start-script"}, function(response) {
+            if (chrome.runtime.lastError) {
+                console.log("chrome.runtime.lastError " + chrome.runtime.lastError.message);
+            }
+            console.log(response.farewell);
+        });
+    })
 }
--- content-script.js
+++ content-script.js
@@ -1,40 +1,8 @@
-
-// chrome.runtime.sendMessage(
-//     {message: "hi"},
-//     (response) => {
-//         console.log('response.message ', response.message);
-//     }
-// );
-
-chrome.runtime.onMessage.addListener((message,sender, sendResponse) => {
-        console.log(message);
-        sendResponse({message: "goodbye"});
-        // if (request.greeting == "start-script"){
-        //     sendResponse({farewell: "goodbye"});
-        //     console.log(request.greeting);
-        // }
-    });
-
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         console.log(request);
-//         if (request.greeting == "start-script"){
-//             sendResponse({farewell: "goodbye"});
-//             console.log(request.greeting);
-//         }
-//     });
-
-
-// chrome.runtime.onMessage.addListener(
-//     function(request, sender, sendResponse) {
-//         if(request.greeting === "start-script") console.log('response received', request.payload)
-//         sendResponse({status: "done"});
-//         return true;
-//     }
-// )
-
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     console.log("content script got a message" + request);
-//     sendResponse("world");
-// });
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request);
+        if (request.greeting == "start-script"){
+            sendResponse({farewell: "goodbye"});
+        }
+    }
+);
